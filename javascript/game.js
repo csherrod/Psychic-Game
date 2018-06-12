2e15af801003a185b0e9fdee01dc235dd1e8fa05
Cody Sherrod <w.cody.sherrod@gmail.com>
2018-06-12 16:21:44 -0500

    adding a background image

--- javascript/game.js
+++ javascript/game.js
@@ -1,46 +1,62 @@
 // Psychic Game Variables
 var guessesLeft = 9;
 var lettersGuessed = [];
-var userGuess = null;
+var computerGuess = null;
 var wins = 0;
 var losses = 0;
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-//What the computer picks
-var computerGuess = letters[Math.floor(Math.random() * letters.length)];
+
+var updateGuessesLeft = function() {
+        //This will show the user how many guesses they have left
+        document.querySelector("#guessesLeft").innerHTML = guessesLeft;
+      };
+  
+      var updateComputerGuess = function() {
+          // This will generate the computer guess
+          computerGuess = letters[Math.floor(Math.random() * letters.length)];
+      };
+  
+      var updateGuessesSoFar = function() {
+        // Display User Guesses
+        document.querySelector("#lettersGuessed").innerHTML = lettersGuessed.join(", ");
+      };
 
 //This will reset the game
-var start = function () {
+var reset = function () {
     guessesLeft = 9;
     lettersGuessed = [];
-    computerGuess = letters[Math.floor(Math.random() * letters.length)];
+    updateGuessesLeft();
+    updateComputerGuess();
+    updateGuessesSoFar();
 };
+
+updateComputerGuess();
+updateGuessesLeft();
+
 
 // This will start to listen for user input
 document.onkeyup = function (event) {
+            //If it's a new letter decrease guesses -1
+            guessesLeft--;
     //Makes sure we are listening for only letters and it wasn't previously picked
-    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-    if (lettersGuessed.indexOf(userGuess) < 0 && letters.indexOf(userGuess) >= 0) {
-        lettersGuessed[lettersGuessed.length] = userGuess;
-        //If it's a new letter decrease guesses -1
-        guessesLeft--;
-        //Overwrite html to update the # of guesses left for user
-        document.querySelector("#guessesLeft").innerHTML = guessesLeft;
-        //Will display on page the letters user has input
-        document.querySelector("#lettersGuessed").innerHTML = lettersGuessed;
-    }
+    var userGuess = String.fromCharCode(event.which).toLowerCase();
+    lettersGuessed.push(userGuess);
+    // Then update functions
+    updateGuessesLeft();
+    updateGuessesSoFar();
 
     //Win condition
-    if (computerGuess == userGuess) {
+    if (computerGuess === userGuess) {
         wins++;
         //Overwrite html for wins
         document.querySelector("#wins").innerHTML = wins;
-        start();
+        reset();
     }
     //Loss condition
-    if (guessesLeft == 0) {
+    if (guessesLeft === 0) {
         losses++;
         document.querySelector("#losses").innerHTML = losses;
-        start();
+        reset();
     }
 };
